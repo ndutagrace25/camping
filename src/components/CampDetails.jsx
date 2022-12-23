25820c5f86eb9360a46300a9fb6d68e13c8b0eca
Grace Nduta <ndutagrace25@gmail.com>
2022-12-23 11:39:23 +0300

    added activity details

--- src/components/CampDetails.jsx
+++ src/components/CampDetails.jsx
@@ -4,6 +4,7 @@ import GoogleMapReact from "google-map-react";
 import { allCamps } from "../data/data";
 import queryString from "query-string";
 import { useLocation } from "react-router-dom";
+import Fade from "react-reveal/Fade";
 
 const CampDetails = (props) => {
   const cutrrentId = queryString.parse(useLocation().search);
@@ -19,6 +20,19 @@ const CampDetails = (props) => {
     },
     zoom: 12,
   };
+
+  const activities = currentCamp.activites.map((item, index) => {
+    return (
+      <div key={index} className="py-3 d-flex flex-row">
+        <Fade bottom delay={400}>
+          <span>
+            <i className="fa-solid fa-gem me-2 text-info"></i>
+          </span>
+          {item.name}
+        </Fade>
+      </div>
+    );
+  });
 
   const AnyReactComponent = ({ text }) => (
     <div
@@ -47,26 +61,175 @@ const CampDetails = (props) => {
           />
         </GoogleMapReact>
       </div>
+
       <div
-        className=" col-md-12 d-flex justify-content-center"
+        className="bg-white shadow p-3 rounded"
         style={{ fontFamily: "Carviar Dreams" }}
       >
-        <div className="p-2 d-flex justify-content-center align-items-center rounded bg-white col-md-3 shadow">
-          Get directions to {currentCamp.name}
-          <a
-            href={`https://www.google.com/maps/search/?api=1&query=${currentCamp.lat},${currentCamp.lon}`}
-            target="_blank"
-            className="fas fa-external-link-alt text-primary fa-lg ms-2"
-            style={{ cursor: "pointer" }}
-            rel="noreferrer"
-          >
-            {""}
-          </a>
+        <div
+          className=" col-md-12 d-flex justify-content-center"
+          style={{ fontFamily: "Carviar Dreams" }}
+        >
+          <div className="pb-3 d-flex justify-content-center align-items-center  col-md-3 ">
+            Get directions to {currentCamp.name}
+            <a
+              href={`https://www.google.com/maps/search/?api=1&query=${currentCamp.lat},${currentCamp.lon}`}
+              target="_blank"
+              className="fas fa-external-link-alt text-primary fa-lg ms-2"
+              style={{ cursor: "pointer" }}
+              rel="noreferrer"
+            >
+              {""}
+            </a>
+          </div>
         </div>
-      </div>
-
-      <div className="bg-white shadow my-3">
-        
+        <div className="d-flex justify-content-center">
+          <h5 style={{ fontFamily: "Carviar Dreams Bold" }}>
+            {currentCamp.name}
+          </h5>
+        </div>
+        <div className="d-flex justify-content-bentween pt-3 col-md-12 activity-mobile">
+          <div className="col-md-6 desktop-view">
+            <div className="d-flex justify-content-between flex-wrap ">
+              <div className="activity-image zoom">
+                <Fade left delay={500}>
+                  <img
+                    alt="imagecamp"
+                    src={currentCamp.image_one}
+                    className="img-fluid  rounded"
+                  />
+                </Fade>
+              </div>
+              <div className="activity-image zoom">
+                <Fade left delay={300}>
+                  <img
+                    alt="imagecamp"
+                    src={currentCamp.image_two}
+                    className="img-fluid rounded"
+                  />
+                </Fade>
+              </div>
+              <div className="activity-image zoom">
+                <Fade left delay={500}>
+                  <img
+                    alt="imagecamp"
+                    src={currentCamp.image_three}
+                    className="img-fluid rounded"
+                  />
+                </Fade>
+              </div>
+              <div className="activity-image zoom">
+                <Fade left delay={500}>
+                  <img
+                    alt="imagecamp"
+                    src={currentCamp.image_four}
+                    className="img-fluid rounded"
+                  />
+                </Fade>
+              </div>
+            </div>
+          </div>
+          <div className="mobile-view px-3 rounded">
+            <Fade left delay={400}>
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide"
+                data-bs-ride="true"
+              >
+                <div className="carousel-indicators">
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="0"
+                    className="active"
+                    aria-current="true"
+                    aria-label="Slide 1"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="1"
+                    aria-label="Slide 2"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
+                  <button
+                    type="button"
+                    data-bs-target="#carouselExampleIndicators"
+                    data-bs-slide-to="2"
+                    aria-label="Slide 3"
+                  ></button>
+                </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img
+                      src={currentCamp.image_one}
+                      className="d-block w-100"
+                      alt={currentCamp.image_one}
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={currentCamp.image_two}
+                      className="d-block w-100"
+                      alt={currentCamp.image_two}
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={currentCamp.image_three}
+                      className="d-block w-100"
+                      alt={currentCamp.image_three}
+                    />
+                  </div>
+                  <div className="carousel-item">
+                    <img
+                      src={currentCamp.image_three}
+                      className="d-block w-100"
+                      alt={currentCamp.image_three}
+                    />
+                  </div>
+                </div>
+                <button
+                  className="carousel-control-prev"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="prev"
+                >
+                  <span
+                    className="carousel-control-prev-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                  className="carousel-control-next"
+                  type="button"
+                  data-bs-target="#carouselExampleIndicators"
+                  data-bs-slide="next"
+                >
+                  <span
+                    className="carousel-control-next-icon"
+                    aria-hidden="true"
+                  ></span>
+                  <span className="visually-hidden">Next</span>
+                </button>
+              </div>
+            </Fade>
+          </div>
+          <div className="col-md-6">
+            <div className="d-flex justify-content-center py-3 fw-bold">
+              Activities At {currentCamp.name}
+            </div>
+            <div className="d-flex justify-content-center flex-column align-items-center">
+              {activities}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
