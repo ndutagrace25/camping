b8cd2aded2591a8ab6d41f7444e4af175c59b9b3
Grace Nduta <ndutagrace25@gmail.com>
2022-12-23 12:11:58 +0300

    changed navbar

--- src/common/Navbar.jsx
+++ src/common/Navbar.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-expand-lg bg-info text-black">
+    <nav className="navbar navbar-expand-lg bg-secondary text-white">
       <div className="container">
         <Link
           className="navbar-brand"
